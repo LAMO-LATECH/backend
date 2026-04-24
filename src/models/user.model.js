c92398678846dc6a4c3103e12bcb453cd85e3737
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2026-04-24 15:29:48 -0700

    feat: create user model & hash password

---
+++ src/models/user.model.js
@@ -0,0 +1,86 @@
+import mongoose, { Schema } from "mongoose";
+import bcyrpt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unqiue: true,
+      lowercase: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      minLength: 8,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unqiue: true,
+      lowercase: true,
+      trim: true,
+    },
+    picture: {
+      type: String,
+    },
+    authProvider: {
+      type: String,
+      enum: ["local", "google"],
+      required: true,
+      default: "local",
+    },
+    googleId: {
+      type: String,
+      sparse: true,
+      unique: true,
+    },
+    points: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    routesAccepted: {
+      type: Number,
+      default: 0,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    refreshToken: {
+      type: String,
+      select: false,
+    },
+    refreshTokenExpiry: {
+      type: Date,
+      select: false,
+    },
+    lastActive: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: { createdAt: "createdOn", updatedAt: "updatedAt" },
+  },
+);
+
+// before saving any password we need to hash it
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcyrpt.hash(this.password, 10);
+  next();
+});
+
+// compare passwords
+userSchema.methods.comparePassword = async function (password) {
+  return await bcyrpt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
