b36dc89feaec24ff6bf353e7b8fa4bad143c7646
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2026-04-25 00:29:50 -0700

    feat: add routes and dependencies

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller.js";
+import {
+  registerUser,
+  loginUser,
+  refreshUser,
+  logoutUser,
+} from "../controllers/auth.controller.js";
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-
+router.route("/refresh").post(refreshUser);
+router.route("/logout").post(logoutUser);
 export default router;
