857ddea745699aba38ca38bc78ceff86dfc21771
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2026-04-25 14:30:32 -0700

    add user router to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
-
+app.use("/api/v1/users", userRouter);
 export default app;
