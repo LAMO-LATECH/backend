5ed91e60e6c11aa5c35746048ced9e920abb097d
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2026-04-25 00:27:43 -0700

    feat: add token service
    generate a access token and generate a refresh token

---
+++ src/services/token.service.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+
+const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+
+if (!ACCESS_SECRET || !REFRESH_SECRET) {
+  throw new Error(
+    "JWT_ACCESS_SECRET and JWT_REFRESH_SECRET must be set in .env!",
+  );
+}
+
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { userId: user._id ?? user.id, role: user.role },
+    ACCESS_SECRET,
+    { expiresIn: "15m" },
+  );
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ userId: user._id ?? user.id }, REFRESH_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+const verifyAcessToken = (token) => {
+  return jwt.verify(token, ACCESS_SECRET);
+};
+
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, REFRESH_SECRET);
+};
+
+export {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAcessToken,
+  verifyRefreshToken,
+};
